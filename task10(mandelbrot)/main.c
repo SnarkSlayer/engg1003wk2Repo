458621bca7cd5a130dc799dd9b21509033ccff2e
Jakob Flannery <c3355432@uon.edu.au>
2020-03-03 15:42:24 +1100

    Damn I'm good, 2 tries :3

--- task10(mandelbrot)/main.c
+++ task10(mandelbrot)/main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int iterations = 0;
+    float x, y, x1, y1, xtemp;
+    printf("Enter a complex number in the format (x + [-]yi)\n");
+    scanf("%f + %fi", &x, &y);
+
+    while((x1*x1 + y1*y1 < 4) && iterations < 1000) {
+        xtemp = x1*x1 - y1*y1 + x;
+        y1 = 2*x1*y1 + y;
+        x1 = xtemp;
+        iterations++;
+    }
+
+    printf("Total iterations: %d", iterations);
+
     return 0;
 }
